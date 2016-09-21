da17103e2044e8327c039e0ceffb33dd3d573b47
unknown <adamb14@ru.is>
2016-09-21 12:25:16 +0000

    Test folder with test class

---
+++ src/test/java/TestUserService.java
@@ -0,0 +1,15 @@
+/**
+ * Created by AdamHaukur on 9/21/2016.
+ */
+public class TestUserService {
+
+    //TODO test for user service
+
+    /**
+     Test if adding and getting users works right
+         Add user, first name, lastname and userId
+         Add user that fails
+         Get user that exists
+         Check if getting a user that does not exists works correctly
+     **/
+}
